0011c50b1ed36382e80f61c1ff7a8ed6b43ca458
Guilherme Nobrega <gbn6699@gmail.com>
2023-07-25 16:21:43 -0300

    Bootstrap-5-course

---
+++ class30/class30.js
@@ -0,0 +1,5 @@
+const toastElList = document.querySelectorAll(".toast");
+const toastList = [...toastElList].map((toastEl) => {
+    const toast = new bootstrap.Toast(toastEl, {});
+    toast.show();
+});
